1eca94c129c0bf10043516f7623525041de4f9e6
AlmorabeaO <128887305+AlmorabeaO@users.noreply.github.com>
2023-07-04 08:48:41 +0300

    Delete process_malicous.sh

--- 3-Feature_Extraction_Shell_Script/process_malicous.sh
+++
@@ -1,5 +0,0 @@
-for ((i = 1 ; i <= 500 ; i++)); do
-  cd /home/mypc/Desktop/Malicious_Pcaps/; /opt/zeek/bin/zeek -r maliciousDataset$i.pcap;
-  cat /home/mypc/Desktop/Malicious_Pcaps/conn.log | /opt/zeek/bin/zeek-cut proto duration orig_pkts  | awk '$1 == "icmp"' | sed  "s/\s/,/g" | sed 's/^icmp,//'   | sed "s/$/,1 /g" >> /home/mypc/Desktop/final_Malicious.csv;
-  echo $i;
-done
